04f0ec9e9ed0675a3a66f4752f816f5fabf2c8ca
sziki12 <szikilevi01@gmail.com>
2023-10-22 23:36:06 +0200

    Getting Started
    Discovering how Shared Preferences and SettingsFragment works

---
+++ Gyakorlas/app/src/main/java/hu/bme/aut/android/gyakorlas/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package hu.bme.aut.android.gyakorlas
+
+import android.content.Context
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
