ba619668c1bdb45bf3873a081653083fb5f8d58a
sziki12 <szikilevi01@gmail.com>
2023-11-15 11:48:26 +0100

    Restructured
    Passing RequestBody with json in it to server

--- Gyakorlas/app/build.gradle.kts
+++ Gyakorlas/app/build.gradle.kts
@@ -34,11 +34,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        //sourceCompatibility = JavaVersion.VERSION_1_8
+        //targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility =  JavaVersion.VERSION_17
+        targetCompatibility =  JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        //jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         buildConfig = true
@@ -51,14 +54,14 @@ dependencies {
 
     val retrofit_version = "2.9.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation("com.squareup.retrofit2:converter-moshi:$retrofit_version")
-
-    //val moshi_version = "1.9.2"
-    //implementation("com.squareup.moshi:moshi-kotlin:$moshi_version")
-    //ksp("com.squareup.moshi:moshi-kotlin-codegen:$moshi_version")
 
     val gson_version ="2.5.0"
     implementation("com.squareup.retrofit2:converter-gson:$gson_version")
+    implementation("com.google.code.gson:gson: $gson_version")
+
+    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
--- Gyakorlas/app/src/main/java/hu/bme/aut/android/gyakorlas/retrofit/DataAccess.kt
+++ Gyakorlas/app/src/main/java/hu/bme/aut/android/gyakorlas/retrofit/DataAccess.kt
@@ -1,25 +1,28 @@
 package hu.bme.aut.android.gyakorlas.retrofit
 
 import android.util.Log
-import android.widget.Button
 import com.google.gson.Gson
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-import com.squareup.moshi.Moshi
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okio.Buffer
 import org.json.JSONArray
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
-import retrofit2.http.Query
 import java.io.IOException
 
 
@@ -37,23 +40,25 @@ class DataAccess {
                 //.baseUrl(BuildConfig.NEWS_BASE_URL)
                 .client(client)
                 //.addConverterFactory(ScalarsConverterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
+                //.addConverterFactory(GsonConverterFactory.create())
                 //.addConverterFactory(Json.asConverterFactory("application/json".toMediaType()) )
                 /*.addConverterFactory(
                         Json{ ignoreUnknownKeys = true }.asConverterFactory(
                             "application/json".toMediaType()) )*/
                 .build()
 
-            /*val retrofit = Retrofit.Builder()
-                .baseUrl("https://travelist-red.vercel.app/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            */
+            // Create JSON using JSONObject
+            val jsonObject = JSONObject()
+            jsonObject.put("email", user.email)
+            jsonObject.put("password", user.password)
+            // Convert JSONObject to String
+            val jsonObjectString = jsonObject.toString()
+
+            val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
+
             val userAPI = retrofit.create(UserAccessAPI::class.java)
 
-            //if(user.email!=null&&user.password!=null)
-            //{
-                val userCall = userAPI.loginUser(user)
+                val userCall = userAPI.loginUser(requestBody)
                 userCall.enqueue(object: Callback<Void> {
                     override fun onFailure(call: Call<Void>, t: Throwable) {
                         //Faliure
@@ -78,9 +83,6 @@ class DataAccess {
                         }
                     }
                 })
-
-           // }
-
         }
     }
 }
@@ -90,49 +92,47 @@ data class UserData(
     val password: String
 )
 
-
-//data class User(@Json(name = "email") val email: String?,
-          // @Json(name = "password") val password: String?,
-          // @Json(name = "message")val message:String?)
-
-/*data class User(
-    @Json(name = "email")
-    val email: String,
-    @Json(name = "password")
-    val password: String,
-    @Json(name = "message")
-    val message: String?
-)*/
-
-
-
 interface UserAccessAPI {
     //@Headers("Content-Type: application/json")
     @POST("login")
-    fun loginUser(@Body user:UserData): Call<Void>
+    fun loginUser(@Body  requestBody: RequestBody): Call<Void>
 }
 
 class LoggingInterceptor : Interceptor {
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
-        var gson = Gson()
         val request: Request = chain.request()
         val t1 = System.nanoTime()
-        Log.d("TAG_HTTP", java.lang.String.format("Sending request %s on %s%n%s",
-            request.url(), chain.connection(), request.headers()))
-        Log.d("TAG_HTTP","Request body: "+request.body().toString())
-        Log.d("TAG_HTTP","Response body: "+gson.fromJson(request.body(),UserData::class.java))
+        Log.d(
+            "TAG_HTTP", java.lang.String.format(
+                "Sending request %s on %s%n%s",
+                request.url, chain.connection(), request.headers
+            )
+        )
+        Log.d("TAG_HTTP", "Request body: " + parseRequest(request))
 
         val response: okhttp3.Response = chain.proceed(request)
         val t2 = System.nanoTime()
-        Log.d("TAG_HTTP", java.lang.String.format("Received response for %s in %.1fms%n%s",
-            response.request().url(), (t2 - t1) / 1e6, response.headers()))
-        //Log.d("TAG_HTTP","Response body: "+gson.fromJson(response.body().toString(),UserData::class.java).email)
-        /*val resBody = JSONArray(response.body())
-        for(i in 0..resBody.length())
-        {
-            Log.d("TAG_HTTP","Request body: "+resBody[i])
-        }*/
+        Log.d(
+            "TAG_HTTP", java.lang.String.format(
+                "Received response for %s in %.1fms%n%s",
+                response.request.url, (t2 - t1) / 1e6, response.headers
+            )
+        )
         return response
+    }
+
+    private fun parseRequest(request: Request):String
+    {
+        val buffer = Buffer()
+        try {
+            val copy = request.newBuilder().build()
+            copy.body?.writeTo(buffer)
+        }
+        catch(e:IOException)
+        {
+            Log.d("TAG_HTTP","Parse Error")
+        }
+        return buffer.readUtf8()
     }
 }
