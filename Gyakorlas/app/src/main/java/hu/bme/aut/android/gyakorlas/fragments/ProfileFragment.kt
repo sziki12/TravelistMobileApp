fae50b79c6eedd2ce4fd0924e55abe1babb5b59d
eszterzolcsak <zolcsak.eszter@edu.bme.hu>
2023-11-29 22:31:29 +0100

    ProfileFragment: profile picture can be uploaded from gallery/camera

--- Gyakorlas/app/src/main/java/hu/bme/aut/android/gyakorlas/fragments/ProfileFragment.kt
+++ Gyakorlas/app/src/main/java/hu/bme/aut/android/gyakorlas/fragments/ProfileFragment.kt
@@ -1,20 +1,32 @@
 package hu.bme.aut.android.gyakorlas.fragments
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import android.widget.TextView
 import androidx.core.content.edit
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
 import hu.bme.aut.android.gyakorlas.R
 import hu.bme.aut.android.gyakorlas.databinding.FragmentProfileBinding
+import hu.bme.aut.android.gyakorlas.permission.PermissionHandler
 
 class ProfileFragment : Fragment() {
     private lateinit var binding : FragmentProfileBinding
+    private var popupWindow: PopupWindow? = null
+    var SELECT_PICTURE = 200
+    var REQUEST_IMAGE_CAPTURE = 300
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +39,11 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.btnUploadNewPicture.setOnClickListener {
+            showPopupWindow(binding.btnUploadNewPicture)
+        }
+
         return binding.root;
     }
 
@@ -59,6 +76,79 @@ class ProfileFragment : Fragment() {
 
         binding.imgbtnMenu.setOnClickListener {
             findNavController().navigate(R.id.action_profileFragment_to_menuFragment)
+        }
+    }
+
+    private fun showPopupWindow(anchorView: View) {
+        val inflater = LayoutInflater.from(context)
+        val popupView = inflater.inflate(R.layout.popup_window, null)
+
+        val width = LinearLayout.LayoutParams.MATCH_PARENT
+        val height = LinearLayout.LayoutParams.WRAP_CONTENT
+        val focusable = true
+
+        popupWindow = PopupWindow(popupView, width, height, focusable)
+        popupWindow?.showAtLocation(anchorView, Gravity.BOTTOM, 0, 0)
+
+        val uploadPhoto = popupView.findViewById<TextView>(R.id.tvUploadPhoto)
+        val takePhoto = popupView.findViewById<TextView>(R.id.tvTakePhoto)
+
+
+        uploadPhoto.setOnClickListener() {
+            this.activity?.let {
+                PermissionHandler.requestPermission(
+                    it,
+                    PermissionHandler.READ_EXTERNAL_STORAGE_REQUEST_CODE,
+                    {
+                        startSelectPicture()
+                        Log.i("PERMISSION", "Access to photos enabled")
+                    })
+                {
+                    Log.i("PERMISSION", "Access to photos disabled")
+                }
+            }
+            popupWindow?.dismiss()
+        }
+
+        takePhoto.setOnClickListener(){
+            this.activity?.let{
+                PermissionHandler.requestPermission(
+                    it,
+                    PermissionHandler.CAMERA_ACCESS_REQUEST_CODE,
+                    {
+                        startTakePictureIntent()
+                        Log.i("PERMISSION", "Access to camera enabled")
+                    })
+                {
+                    Log.i("PERMISSION", "Access to camera disabled")
+                }
+            }
+            popupWindow?.dismiss()
+        }
+    }
+
+    private fun startSelectPicture(){
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, SELECT_PICTURE)
+    }
+
+    private fun startTakePictureIntent(){
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == SELECT_PICTURE) {
+            data?.data?.let { uri ->
+                val bitmap = MediaStore.Images.Media.getBitmap(context?.contentResolver, uri)
+                binding.ivProfilePicture.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 450, 450, true))
+            }
+        }
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_IMAGE_CAPTURE){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.ivProfilePicture.setImageBitmap(Bitmap.createScaledBitmap(imageBitmap, 450, 450, true))
         }
     }
 }
