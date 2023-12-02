634649342b78dbfee065aa6c5aa70b083e0e32cb
eszterzolcsak <zolcsak.eszter@edu.bme.hu>
2023-12-02 18:45:09 +0100

    RecyclerView in RequestHelpFragment done

--- Gyakorlas/app/src/main/java/hu/bme/aut/android/gyakorlas/fragments/RequestHelpFragment.kt
+++ Gyakorlas/app/src/main/java/hu/bme/aut/android/gyakorlas/fragments/RequestHelpFragment.kt
@@ -15,10 +15,13 @@ import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.maps.model.LatLng
 import hu.bme.aut.android.gyakorlas.R
 import hu.bme.aut.android.gyakorlas.comment.Comment
 import hu.bme.aut.android.gyakorlas.comment.NewCommentDialog
 import hu.bme.aut.android.gyakorlas.databinding.FragmentRequestHelpBinding
+import hu.bme.aut.android.gyakorlas.location.LocationService
+import hu.bme.aut.android.gyakorlas.mapData.MapMarker
 import hu.bme.aut.android.gyakorlas.requestHelp.HelpMessage
 import hu.bme.aut.android.gyakorlas.requestHelp.HelpMessagesAdapter
 import hu.bme.aut.android.gyakorlas.requestHelp.RequestHelpDialogFragment
@@ -30,6 +33,7 @@ class RequestHelpFragment : Fragment(), RequestHelpListener {
     private lateinit var binding : FragmentRequestHelpBinding
     private lateinit var helpMessagesAdapter: HelpMessagesAdapter
     private var helpMessages:ArrayList<HelpMessage> = ArrayList()
+    var markers: ArrayList<MapMarker> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,18 +51,10 @@ class RequestHelpFragment : Fragment(), RequestHelpListener {
         binding.rvMessages.layoutManager = LinearLayoutManager(this.context)
         binding.rvMessages.adapter = helpMessagesAdapter
 
-//        val pulseAnimation = AnimationUtils.loadAnimation(this.context, R.anim.pulse_animation)
-//        var isPulsing = false
+        val pulseAnimation = AnimationUtils.loadAnimation(this.context, R.anim.pulse_animation)
+        binding.ibRequestHelp.startAnimation(pulseAnimation)
 
         binding.ibRequestHelp.setOnClickListener() {
-//            if (!isPulsing) {
-//                binding.ibRequestHelp.startAnimation(pulseAnimation)
-//                isPulsing = true
-//            }
-//            else {
-//                binding.ibRequestHelp.clearAnimation()
-//                isPulsing = false
-//            }
             val dialogFragment = RequestHelpDialogFragment()
             dialogFragment.listener = this
             dialogFragment.show(
@@ -72,11 +68,22 @@ class RequestHelpFragment : Fragment(), RequestHelpListener {
     }
 
     override fun onRequestHelp(helpMessage: HelpMessage) {
-        Log.i("MESSAGE", helpMessage.message)
         helpMessages.add(helpMessage)
 
-        for(h in helpMessages)
-            Log.i("HELPMESSAGES", h.message)
+        val selfLatLng = LocationService.currentLocation?.latitude?.let { LocationService.currentLocation?.longitude?.let { it1 ->
+            LatLng(it,
+                it1
+            )
+        } }
+
+        if (selfLatLng != null) {
+            Log.i("SELFMARKER", selfLatLng.latitude.toString() + " " + selfLatLng.longitude.toString())
+            markers.add(MapMarker(helpMessage.username, selfLatLng.latitude, selfLatLng.longitude))
+        }
+
+
+        for (m in markers)
+            Log.i("RHMARKERS", m.name)
 
         helpMessagesAdapter.update(helpMessages)
     }
